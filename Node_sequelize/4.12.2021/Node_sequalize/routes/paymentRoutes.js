c0f13d9b7279ee369ce6bcea729d6d1ce1a6366d
HarshiniRamalinga <91422976+HarshiniRamalinga@users.noreply.github.com>
2021-12-10 18:46:38 +0530

    Delete Node_sequelize/4.12.2021 directory

--- Node_sequelize/4.12.2021/Node_sequalize/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const {moviePayment,orderMovie}=require("../controllers/paymentController");
-
-const payrouter=require("express").Router();
-
-payrouter.post("/payment",moviePayment)
-payrouter.get("/order/",orderMovie)
-module.exports=payrouter
--- Node_sequelize/4.12.2021/Node_sequalize/routes/usermovieRoutes.js
+++
@@ -1,7 +0,0 @@
-const {getUserMovies} =require("../controllers/usermoviesController")
-
-const mymovierouter = require('express').Router()
-
-mymovierouter.get("/mymovies",getUserMovies)
-
-module.exports=mymovierouter;
