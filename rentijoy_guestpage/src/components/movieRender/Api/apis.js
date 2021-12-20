804104a20f53f76a136771315cc3ce0521fe73e6
Vinith-arun <91451785+Vinith-arun@users.noreply.github.com>
2021-12-20 10:17:51 +0530

    Update apis.js

--- rentijoy_guestpage/src/components/movieRender/Api/apis.js
+++ rentijoy_guestpage/src/components/movieRender/Api/apis.js
@@ -6,3 +6,6 @@ export async function fetchMovieList(listname) {
 export async function fetchMovieDetail(id) {
     return await axios.get(`http://localhost:4000/movie/findById?id=${id}`)
 }
+export async function fetchUserMovieDetail(id,mid) {
+    return await axios.get(`http://localhost:4000/user/movies/mymovies?id=${id}&movieid=${mid}`)
+}
